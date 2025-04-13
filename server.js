f66ab61c0aa5c63a0cbcaed82de0d4b3f6ebced9
shristy <shristychaudhary4679@gmail.com>
2025-04-13 11:29:16 +0530

    Add api logic for progress

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/user");
+const UserProgress = require("./models/userProgress");
 const app = express();
 const PORT = 8080;
 
@@ -25,6 +26,32 @@ app.post("/api/users", async (req, res) => {
       } catch (err) {
         res.status(500).json({ error: err.message });
       }
+});
+
+app.post("/api/progress", async (req, res) => {
+  // const { userId, videoId, segment, videoLength, currentProgress } = req.body;
+    const { userId, videoId, lastWatched, videoLength } = req.body;
+  try {
+    let userProgress = await UserProgress.findOne({ userId, videoId });
+
+    if (!userProgress) {
+        userProgress = new UserProgress({
+            userId,
+            videoId,
+            lastWatched,
+            videoLength
+        });
+    } else {
+        userProgress.lastWatched = lastWatched;
+    }
+
+    await userProgress.save();
+    res.status(200).json({message: "Progress updated successfully"});
+
+} catch (error) {
+    console.error("Error updating progress:", error);
+    res.status(500).json({ error: "Failed to update progress" });
+}
 });
 
 app.listen(PORT, () => {
